16bf5b1871647f4f9b34ef6dbe8be45b30da5144
c-ashok.ghadge <c-ashok.ghadge@ILPT2766.ICERTIS.COM>
2021-06-17 16:11:54 +0530

    p

--- EndToEndTesting/src/test/java/testPackage/NewTest01.java
+++ EndToEndTesting/src/test/java/testPackage/NewTest01.java
@@ -7,7 +7,7 @@ import org.testng.annotations.AfterTest;
 public class NewTest01 {
   @Test
   public void FirstHello() {
-	  System.out.println("HI............. I am first Hello");
+	  System.out.println("HI............... I am first Hello");
   }
   @BeforeTest
   public void beforeTest() {
